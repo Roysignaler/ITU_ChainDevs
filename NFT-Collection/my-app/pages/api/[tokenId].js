a48da61e541bb5b6990deef88974d1c5f29354db
Roysignaler <98877973+Roysignaler@users.noreply.github.com>
2022-10-08 02:32:10 +0200

    Update [tokenId].js

--- NFT-Collection/my-app/pages/api/[tokenId].js
+++ NFT-Collection/my-app/pages/api/[tokenId].js
@@ -12,5 +12,34 @@ export default function handler(req, res) {
       name: "Chain Dev #" + tokenId,
       description: "Chain Dev is a collection of ITU images for BCC developers",
       image: image_url + "ITU_" + tokenId + ".jpg",
+      "attributes": [
+        {
+          "trait_type": "Base", 
+          "value": "Blockchain Developer"
+        },
+        {
+            "trait_type": "University", 
+            "value": "IT-University of Copenhagen"
+        },
+        {
+            "trait_type": "Event", 
+            "value": "Blockchain Coding Café"
+        }, 
+        {
+          "display_type": "boost_number", 
+          "trait_type": "Coding Power", 
+          "value": 99
+        }, 
+        {
+          "display_type": "boost_percentage", 
+          "trait_type": "Stamina Increase", 
+          "value": 5
+        }, 
+        {
+          "display_type": "number", 
+          "trait_type": "Generation", 
+          "value": 1
+        }
+      ]
     });
   }
