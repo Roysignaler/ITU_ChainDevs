49e3ccb6625fc6fa4c8304072655e4069a31a97f
Roysignaler <98877973+Roysignaler@users.noreply.github.com>
2022-11-01 09:04:06 +0100

    Update index.js

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xAE6a88aC27C2F0E35Fcf920659B6Aa7d3b2Af95D";
+const WHITELIST_CONTRACT_ADDRESS = "0xB56eC9d68E18d5D149A9C013f381B038a213180";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://itu-chaindevs-nft.vercel.app/api/";
 
